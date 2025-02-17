3623e53e79f842fbf39aa886af8b4c6870cda0bd
Merimer08 <marialolu08@gmail.com>
2025-02-17 14:29:57 +0100

    stilo login

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -6,6 +6,31 @@
     <!--   -->
     <img class="h-16 max-h-full bg-white" src="{{asset("img/logo.png")}}" alt="logo">
     <h1 class="text-5xl text-orange-500 ">DESARROLLO WEB</h1>
+    @guest
+            <form action="">
+                <a href="{{route('login')}}" class="btn btn-sm btn-primary btn-outline">Login</a>
+                <button class="btn btn-sm">Register</button>
+            </form>
+        @endguest
+        @auth
+            {{auth()->user()->name}}
+                <form action="{{route("logout")}}" method="post">
+                    @csrf
+                    <input class="btn btn-sm" type="submit" value="logout">
+                </form>
+
+        @endauth
+
+    </div>
+
+</header>
+{{--Diseño para moviles--}}
+<header class="md:hidden bg-header flex flex-col justify-between
+ items-center  p-3
+">
+    {{--    --}}
+    <!--   -->
+    <img  class="m-4 h-16 max-h-full bg-white" src="{{asset("images/logo.png")}}" alt="logo">
     <div>
         @guest
             <form action="">
