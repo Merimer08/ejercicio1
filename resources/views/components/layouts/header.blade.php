70c2ada7243ddeca54e086db0332a0e2fe50b4c2
Merimer08 <marialolu08@gmail.com>
2025-02-21 09:17:11 +0100

    menu hamburguesa en el login y register

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,57 +1,32 @@
-
-<header class="h-15v bg-header flex flex-row justify-between
- items-center  p-3
-">
-    {{-- DONDE ESTA EL DE LOGIN   --}}
-    <!--   -->
+<header class="h-15v bg-header flex flex-row items-center justify-between p-3 relative">
+{{-- otra forma de poner anotaciones   --}}
+<!--   -->   
+<!-- Logo -->
     <a href="{{ route('main') }}">
-    <img class="h-16 max-h-full bg-white" src="{{ asset('img/logo.png') }}" alt="logo">
+        <img class="h-16 max-h-full bg-white" src="{{ asset('img/logo.png') }}" alt="logo">
     </a>
-    <h1 class="text-5xl  text-orange-500 text-center">DESARROLLO WEB </h1>
-    
-   <imput type= 
-    @guest
-            <form action="">
-                <a href="{{route('login')}}" class="btn btn-sm btn btn-outline text-black hover:bg-orange-300 hover:text-black">Login</a>
-                <a href="{{route('register')}}" class="btn btn-sm btn btn-active text-black hover:bg-orange-300 hover:text-black">Register</a>
-                
-            </form>
-        @endguest
-        @auth
-            {{auth()->user()->name}}
-                <form action="{{route("logout")}}" method="post">
+
+    <!-- Título centrado -->
+    <h1 class="text-5xl text-orange-500 absolute left-1/2 transform -translate-x-1/2">DESARROLLO WEB</h1>
+
+    <!-- Menú hamburguesa -->
+    <div class="relative">
+        <input type="checkbox" id="menu-toggler" class="peer hidden">
+        <label for="menu-toggler" class="text-3xl cursor-pointer">&#9778;</label>
+        
+        <!-- Contenedor del menú -->
+        <div class="hidden peer-checked:flex flex-col absolute right-0 top-full bg-white p-3 shadow-lg rounded-lg">
+            @guest
+                <a href="{{route('login')}}" class="btn btn-sm btn-outline text-black hover:bg-orange-300 hover:text-black">Login</a>
+                <a href="{{route('register')}}" class="btn btn-sm btn-active text-black hover:bg-orange-300 hover:text-black">Register</a>
+            @endguest
+            @auth
+                <p>{{auth()->user()->name}}</p>
+                <form action="{{route('logout')}}" method="post">
                     @csrf
                     <input class="btn btn-sm" type="submit" value="logout">
                 </form>
-
-        @endauth
-
+            @endauth
+        </div>
     </div>
-
-</header>
-{{--Diseño para moviles--}}
-<header class="md:hidden bg-header flex flex-col justify-between
- items-center  p-3
-">
-    {{--    --}}
-    <!--   -->
-    <img  class="m-4 h-16 max-h-full bg-white" src="{{asset("images/logo.png")}}" alt="logo">
-    <div>
-        @guest
-            <form action="">
-                <a href="{{route('login')}}" class="btn btn-sm btn-primary btn-outline">Login</a>
-                <button class="btn btn-sm">Register</button>
-            </form>
-        @endguest
-        @auth
-            {{auth()->user()->name}}
-            <form action="{{route("logout")}}" method="post">
-                @csrf
-                <input class="btn btn-sm" type="submit" value="logout">
-            </form>
-
-        @endauth
-
-    </div>
-
 </header>
